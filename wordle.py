5b61b7d84bd636c4411b922e35c43fbfaf75cea3
DGopalani <114025985+DGopalani@users.noreply.github.com>
2023-10-30 08:37:47 -0700

    interactive now

--- wordle.py
+++ wordle.py
@@ -1,28 +1,41 @@
 import requests
 import json
 
-url = 'https://wordle-api.vercel.app/api/wordle'
-myobj = {
-    "guess": "class"
-}
-
-response = requests.post(url, json=myobj)
-
-if response.status_code == 200:
-    # Parse the JSON response
-    data = json.loads(response.text)
-
-    # You can now access the data as a dictionary
-    was_correct = data['was_correct']
-    character_info = data['character_info']
-
-    # Iterate through character_info
-    for info in character_info:
+def display_wordle_result(data, guess):
+    print(f"Guess: {guess}")
+    print(f"Was Correct: {data['was_correct']}")
+    for info in data['character_info']:
         char = info['char']
         in_word = info['scoring']['in_word']
         correct_idx = info['scoring']['correct_idx']
-
         print(f"Character: {char}, In Word: {in_word}, Correct Index: {correct_idx}")
+    print("\n")
 
-else:
-    print("Failed to make the API request. Status code:", response.status_code)
+url = 'https://wordle-api.vercel.app/api/wordle'
+attempts = 0
+max_attempts = 6
+
+while attempts < max_attempts:
+    guess = input(f"Enter guess {attempts + 1}/{max_attempts}: ")
+    
+    myobj = {
+        "guess": guess
+    }
+
+    response = requests.post(url, json=myobj)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        display_wordle_result(data, guess)
+
+        if data['was_correct']:
+            print("You've guessed the word! Congrats!")
+            break
+        else:
+            attempts += 1
+    else:
+        print("Failed to make the API request. Status code:", response.status_code)
+        break
+
+if attempts == max_attempts:
+    print(f"Sorry, you've reached the maximum number of attempts. The word was not guessed.")
